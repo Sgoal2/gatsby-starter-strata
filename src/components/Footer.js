7d70dbba5a37374944333cece38f19b815d70a3d
Sag <sag@Apples-MacBook-Pro.local>
2019-02-16 23:47:11 -0500

    github link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ class Footer extends React.Component {
                 <div className="inner">
                     <ul className="icons">
                         <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
-                        <li><a href="#" className="icon fa-github"><span className="label">Github</span></a></li>
+                        <li><a href="https://github.com/Sgoal2" className="icon fa-github"><span className="label">Github</span></a></li>
                         <li><a href="#" className="icon fa-dribbble"><span className="label">Dribbble</span></a></li>
                         <li><a href="#" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
                     </ul>
