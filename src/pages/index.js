6a0624722906306ccd9ed4b80f24ea129cd0c9ba
Sag <sag@Apples-MacBook-Pro.local>
2019-03-04 23:17:31 -0500

    ..

--- src/pages/index.js
+++ src/pages/index.js
@@ -115,7 +115,7 @@ class HomeIndex extends React.Component {
 
                     <section id="three">
                         <h2>Get In Touch</h2>
-                        <p>Accumsan pellentesque commodo blandi. </p>
+                        <p>Accumsan pellentesque commodo bl. </p>
                         <div className="row">
                             <div className="8u 12u$(small)">
                                 <form method="post" action="#">
